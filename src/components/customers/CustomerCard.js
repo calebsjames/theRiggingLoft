304829d69ae28677621602f03299ecb149d1fbcd
Caleb James <calebsjames@gmail.com>
2021-03-19 11:26:30 -0500

    delete customers functionality added

--- src/components/customers/CustomerCard.js
+++ src/components/customers/CustomerCard.js
@@ -1,12 +1,24 @@
 import React, { useContext } from "react"
-import { Link } from "react-router-dom"
-
+import { Link, useHistory } from "react-router-dom"
+import { CustomerContext } from "./CustomerProvider"
 
 
 
 
 export const CustomerCard = ({ customerInstance }) => {
-  
+    
+    const history = useHistory()
+
+    const { getCustomers, deleteCustomer } = useContext(CustomerContext)
+
+    const handleDelete = () => {
+        deleteCustomer(customerInstance.id)
+          .then(getCustomers)
+          .then(() => {
+            history.push("/customers")
+          })
+      }
+    
     return(<section className="customer">
         <h3 className="customer__name">
           <Link to={`/customers/detail/${customerInstance.id}`}>
@@ -14,6 +26,9 @@ export const CustomerCard = ({ customerInstance }) => {
           </Link>
         </h3>
         <p>{ customerInstance.phone }</p>
+        <button onClick={(handleDelete)}>
+                Delete
+            </button>
         
       
     </section>
